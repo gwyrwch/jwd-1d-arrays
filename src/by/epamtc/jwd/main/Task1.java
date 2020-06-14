dccb3bc9b1cf3be04c3878b81250d733fb526845
gwyrwch <gwyrwch@gmail.com>
2020-06-14 19:18:40 +0300

    task1

---
+++ src/by/epamtc/jwd/main/Task1.java
@@ -0,0 +1,35 @@
+package by.epamtc.jwd.main;
+
+import com.sun.media.sound.InvalidDataException;
+
+import java.util.Arrays;
+
+import static by.epamtc.jwd.util.Util.getRandomIntArray;
+
+public class Task1 {
+    private static int findSpecialMax(int[] nums) {
+        if (nums.length % 2 != 0) {
+            try {
+                throw new InvalidDataException("Invalid array size");
+            } catch (InvalidDataException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        int size = nums.length;
+
+        for (int l = 0, r = size - 1; l < size / 2; l++, r--) {
+            ans = Math.max(ans, nums[l] + nums[r]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[] nums = getRandomIntArray(size);
+
+        System.out.printf("Majority element(without sort) in %s is %d\n", Arrays.toString(nums), findSpecialMax(nums));
+    }
+}

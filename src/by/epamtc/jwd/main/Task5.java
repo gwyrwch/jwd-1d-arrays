74be4583f71ee24dd67e162ccb8aeac914e7d969
gwyrwch <gwyrwch@gmail.com>
2020-06-14 19:27:11 +0300

    task 5

---
+++ src/by/epamtc/jwd/main/Task5.java
@@ -0,0 +1,36 @@
+package by.epamtc.jwd.main;
+
+import java.util.Arrays;
+
+import static by.epamtc.jwd.util.Util.getRandomIntArray;
+
+public class Task5 {
+    private static int findEvenMax(final int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i+= 2) {
+            max = Math.max(nums[i], max);
+        }
+
+        return max;
+    }
+
+    private static int findOddMin(final int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < nums.length; i+= 2) {
+            min = Math.min(nums[i], min);
+        }
+
+        return min;
+    }
+
+    private static long solve(int[] nums) {
+        return findEvenMax(nums) + findOddMin(nums);
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[] nums = getRandomIntArray(size);
+
+        System.out.printf("array: %s\neven max + odd min = %d\n", Arrays.toString(nums), solve(nums));
+    }
+}
